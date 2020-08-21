78f1c763df5713772e1cfc8185e5167f98ba3cd1
Napo <Michaeljnapolitano@gmail.com>
2020-08-20 23:07:29 -0400

    fixed name blank bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,6 +73,7 @@ var startGame = function() {
     for(var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+            
         
             var pickedEnemyObj = enemyInfo[i];
             pickedEnemyObj.health = randomNumber(40, 60);
@@ -151,8 +152,16 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+//fucntion to set name
+var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
